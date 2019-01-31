28d26b40fa952f27e8e4eac9e648e20394631701
Stephan Geulette <s.geulette@imio.be>
2019-01-31 15:55:44 +0100

    Added helper views and adapter to get info in send form

--- src/imio/project/soap2pm/browser/views.py
+++ src/imio/project/soap2pm/browser/views.py
@@ -1,23 +1,50 @@
 ## -*- coding: utf-8 -*-
 
-import base64
+from collective.task.interfaces import ITaskMethods
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+
+
+def object_link(obj, view=''):
+    href = view and "%s/%s" % (obj.absolute_url(), view) or obj.absolute_url()
+    return u'<a href="%s">%s</a>' % (href, safe_unicode(obj.Title()))
 
 
 class ProjectSoapClientView(BrowserView):
     """ Adapts an action or task to prepare data to exchange within imio.pm.wsclient """
 
-    # def getMainFiles(self):
-    #     pc = getToolByName(self.context, 'portal_catalog')
-    #     res = []
-    #     for brain in pc(portal_type='dmsmainfile', path='/'.join(self.context.getPhysicalPath())):
-    #         obj = brain.getObject()
-    #         res.append({'title': obj.title.encode('utf8'),
-    #                     'filename': obj.file.filename.encode('utf8'),
-    #                     'file': base64.b64encode(obj.file.data)})
-    #     return res
-    #
-    # def exampleDecision(self):
-    #     return ("<p>Le collège communal autorise la location de la salle par Mr Jean Menfout à la date du"
-    #             "12/02/2015 de 11h à 18h pour l'organisation d'un anniversaire.</p>")
+    def motivation(self):
+        return ("<p></p>")
+
+    def common_detailed_description(self):
+        out = []
+        out.append(u"<p>OS: %s</p>" % object_link(self.os))
+        out.append(u"<p>OO: %s</p>" % object_link(self.oo))
+        out.append(u"<p>Action: %s</p>" % object_link(self.action))
+        return out
+
+
+class ProjectActionSoapClientView(ProjectSoapClientView):
+
+    def __init__(self, context, request):
+        super(ProjectActionSoapClientView, self).__init__(context, request)
+        self.action = self.context
+        self.oo = self.action.aq_parent
+        self.os = self.oo.aq_parent
+
+    def detailed_description(self):
+        return u''.join(self.common_detailed_description())
+
+
+class ProjectTaskSoapClientView(ProjectSoapClientView):
+
+    def __init__(self, context, request):
+        super(ProjectActionSoapClientView, self).__init__(context, request)
+        self.adapted = ITaskMethods(self.context)
+        self.action = self.adapted.get_highest_task_parent(task=False)
+        self.oo = self.action.aq_parent
+        self.os = self.oo.aq_parent
+
+    def detailed_description(self):
+        out = self.common_detailed_description()
+        return u''.join(out)
